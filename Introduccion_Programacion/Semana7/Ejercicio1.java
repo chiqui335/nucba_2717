fce65e1164df97b96a5b9de3f20e9139c861c806
ChiquiLx <chiquilitob@hotmail.com>
2023-09-25 22:51:05 -0300

    Ejercicios java

---
+++ Introduccion_Programacion/Semana7/Ejercicio1.java
@@ -0,0 +1,60 @@
+package Introduccion_Programacion.Semana7;
+
+import java.util.Scanner;
+
+public class Ejercicio1 {
+    public static void main(String[] args) {
+        // Inicio el scanner
+        Scanner sc = new Scanner(System.in);
+
+        // Pido input de la nota
+        System.out.println("Ingrese una nota");
+        int nota = sc.nextInt();
+
+        // Declaro la variable notaEscrita
+        String notaEscrita;
+
+        // Evalúo la nota
+        switch (nota) {
+            case 10:
+                notaEscrita = "Diez";
+                break;
+            case 9:
+                notaEscrita = "Nueve";
+                break;
+            case 8:
+                notaEscrita = "Ocho";
+                break;
+            case 7:
+                notaEscrita = "Siete";
+                break;
+            case 6:
+                notaEscrita = "Seis";
+                break;
+            case 5:
+                notaEscrita = "Cinco";
+                break;
+            case 4:
+                notaEscrita = "Cuatro";
+                break;
+            case 3:
+                notaEscrita = "Tres";
+                break;
+            case 2:
+                notaEscrita = "Dos";
+                break;
+            case 1:
+                notaEscrita = "Uno";
+                break;
+            case 0:
+                notaEscrita = "Cero";
+                break;
+            default:
+                notaEscrita = "Nota fuera de rango";
+                break;
+        }
+        
+        // Imprimo la nota escrita
+        System.out.println("Nota en palabras: " + notaEscrita);
+    }
+}
---
+++ Introduccion_Programacion/Semana7/ejercicio1_parte2.java
@@ -0,0 +1,5 @@
+package Introduccion_Programacion.Semana7;
+
+public class ejercicio1_parte2 {
+
+    }
