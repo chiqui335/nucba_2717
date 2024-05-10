ae6fa30703c0e0ced8aad1936c6af8f400f28e2c
ChiquiLx <chiquilitob@hotmail.com>
2024-05-09 22:39:20 -0300

    sobrecarga

--- CUATRIMESTRE_2/POO/MiEmpresa2/src/empresa/Cliente.java
+++ CUATRIMESTRE_2/POO/MiEmpresa2/src/empresa/Cliente.java
@@ -1,14 +1,15 @@
-package empresa;
+// package empresa;
 
-public class Cliente {
-	public int id;
-	public String nombre;
-	public String contacto;
-	public char estado;
-	public Direccion direccion;
-	
-	public void mostrarInfoCliente() {
-		System.out.println(id+nombre+contacto+estado+direccion);//muestra la info del cliente
-		
-	}
-}
+// public class Cliente {
+// public int id;
+// public String nombre;
+// public String contacto;
+// public char estado;
+// public Direccion direccion;
+
+// public void mostrarInfoCliente() {
+// System.out.println(id+nombre+contacto+estado+direccion);//muestra la info del
+// cliente
+
+// }
+// }
--- CUATRIMESTRE_2/POO/MiEmpresa2/src/empresaTest/Test.java
+++ CUATRIMESTRE_2/POO/MiEmpresa2/src/empresaTest/Test.java
@@ -1,36 +1,36 @@
-package empresaTest;
+// package empresaTest;
 
-import empresa.Cliente;
-import empresa.Direccion;
-import empresa.Empleado;
+// import empresa.Cliente;
+// import empresa.Direccion;
+// import empresa.Empleado;
 
-public class Test {
+// public class Test {
 
-	public static void main(String[] args) {
-		Empleado e1= new Empleado();	//e1, c1, d1, d2 son variables; el new sirve para "reservar" un espacio en memoria para esa variable
-		Cliente c1= new Cliente();//constructor por defecto(SIN PARAMETROS) (lo inserta el compilador por defecto)
-		Direccion d1= new Direccion();
-		Direccion d2= new Direccion();
-		
-		e1.nombreEmpleado="Ignacio";
-		e1.id=123;
-		e1.salario=60000;
-		e1.apellidoEmpleado="Perez";
-		d1.provincia= "CABA";
-		d1.calle="Corrientes";
-		d1.num=2037;
-		d1.mostrarInfoDireccion();
-		e1.mostrarInfo();
-		c1.nombre="Pedro";
-		c1.id=156;
-		d2.provincia="Buenos Aires";
-		d2.calle="Per�n";
-		d2.num=3245;
-		c1.mostrarInfoCliente();
-		d2.mostrarInfoDireccion();
-		
-		
+// public static void main(String[] args) {
+// Empleado e1= new Empleado(); //e1, c1, d1, d2 son variables; el new sirve
+// para "reservar" un espacio en memoria para esa variable
+// Cliente c1= new Cliente();//constructor por defecto(SIN PARAMETROS) (lo
+// inserta el compilador por defecto)
+// Direccion d1= new Direccion();
+// Direccion d2= new Direccion();
 
-	}
+// e1.nombreEmpleado="Ignacio";
+// e1.id=123;
+// e1.salario=60000;
+// e1.apellidoEmpleado="Perez";
+// d1.provincia= "CABA";
+// d1.calle="Corrientes";
+// d1.num=2037;
+// d1.mostrarInfoDireccion();
+// e1.mostrarInfo();
+// c1.nombre="Pedro";
+// c1.id=156;
+// d2.provincia="Buenos Aires";
+// d2.calle="Per�n";
+// d2.num=3245;
+// c1.mostrarInfoCliente();
+// d2.mostrarInfoDireccion();
 
-}
+// }
+
+// }
